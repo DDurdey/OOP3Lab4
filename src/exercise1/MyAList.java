40ec63b5b76ea3bd13a13b2831cec8432422955a
Edwin Wong <edwin.wong@edu.sait.ca>
2025-11-07 14:46:36 -0700

    Initial constructors for exercise 1

--- src/exercise1/MyAList.java
+++ src/exercise1/MyAList.java
@@ -7,6 +7,16 @@ public class MyAList<E> implements Iterable<E>
 {
 	private ArrayList<E> list;
 
+	// Constructor
+    public MyAList() {
+        this.list = new ArrayList<>();
+    }
+
+    // Constructor with initial capacity
+    public MyAList(int initialCapacity) {
+        this.list = new ArrayList<>(initialCapacity);
+    }
+
 	@Override
 	public Iterator<E> iterator()
 	{
